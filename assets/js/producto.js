ba18486518d777302e31c48ba10fe314b50c5ecc
jorge luis camarillo cristobal <jorge@jorges-MacBook-Pro.local>
2020-08-06 03:24:05 -0500

    ADD interior product

---
+++ assets/js/producto.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.slider-product').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.slider-navproduct'
+    });
+    $('.slider-navproduct').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-product',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,        
+    });
+})
